61ceb7ba6bb6ea200a0e5a2d23f11d5cd7c5a8d7
misu <parkour12019@gmail.com>
2017-11-10 01:54:52 +0900

    mod

--- ffprobe.go
+++ ffprobe.go
@@ -1,5 +1,29 @@
 package go_ffprobe
 
-func GetFileInfo(fileName string) string {
-	return fileName
+import (
+	"encoding/json"
+	"os/exec"
+)
+
+var (
+	ExecFunc = ExecCmd
+)
+
+func Execute(fileName string) (r Result, err error) {
+	out, err := ExecFunc(fileName)
+
+	if err != nil {
+		return r, err
+	}
+
+	if err := json.Unmarshal(out, r); err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
+
+func ExecCmd(fileName string) ([]byte, error) {
+	return exec.Command("ffprobe",
+		"-v", "quiet", "-print_format", "json", "-show_format", "-show_streams", fileName).Output()
 }
